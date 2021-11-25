52689025d4a348da4de30b23170abac6a8e03047
KhachKara <khachkara@gmail.com>
2021-11-25 16:35:11 +0300

    Lesson 3

--- Michael Lukin/Qt Lesson 03/Lesson_03/mainwindow.cpp
+++ Michael Lukin/Qt Lesson 03/Lesson_03/mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <cmath>
+# include <QMessageBox>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,5 +14,27 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    double x = 0.0, res = 0.0;
+    bool f = false;
+    QString res_text = "";
+
+    x = ui->lineEdit->text().toDouble(&f);
+    if(f) {
+        res = pow(x, 5);
+        res_text = "Ответ: " + QString::number(res);
+        QMessageBox msgBox(this);
+        msgBox.setWindowTitle("Результат");
+        msgBox.setText(res_text);
+        msgBox.exec();
+    } else {
+        res_text = "введён не корректный X";
+        QMessageBox::warning(this, "Результат", res_text);
+    }
+
 }
 
